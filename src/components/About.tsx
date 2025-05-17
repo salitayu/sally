f0844cfda80125136a69c83abab5f80f5cf84987
salitayu <salitanyu@gmail.com>
2025-05-17 10:20:39 -0700

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,14 +6,14 @@ export default function About() {
             <div className="s-about-container">
                 <div className="s-about-content">
                     <h1>About Me</h1>
-                    <p>For as long as I can remember, I have always been intrigued by the world of technology. 
-                       I would spend every weekend at a store as a cashier selling CDs/DVDs/Bluerays, computers, laptops, MP3s and other popular electronics.
+                    <p>Growing up, I was always intrigued by the world of technology. 
+                       Every weekend, I worked as a cashier at an electronics store selling popular items at the time, including CDs/DVDs/Bluerays, computers, laptops, MP3s, digital cameras, printers and much more.
                        My parents gifted me Super Mario games during the holidays, and I would play them all vacation long.
                        My real fascination for tech though, began back in high school when I decided to take a graphic web design elective that was offered at my school.
                        I started my journey by using Adobe Creative Suite to design everything from shirts to yearbooks.
                        Then, I became proficient in HTML and CSS to create my first websites. I remember the first large project that I made was a website to sell blackberry phones, which were trending at the time.
                        I spent hours after class investigating how to create scrolling and navbar animations for my personal website. 
-                       On weekends, I would try to figure out how to make my site more interactive by having a cart and sending emails.
+                       On the weekends, I would try to figure out how to make my site more interactive by having a cart and sending emails.
                        That year, I also interned at a local computer manufacturer as a quality assurance and laptop assembler, where I installed hardware + software onto machines and ensured sound, software, and hardware quality.
                        Back in school, I continued with graphic web design, and later with yearbook design.</p>
                     <p>That was how I discovered PHP and JavaScript. 
